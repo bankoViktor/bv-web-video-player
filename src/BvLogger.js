7600d0ece19c25f5ed8997c5a36269c18de9eeea
Banko Viktor <bankviktor14@gmail.com>
2022-03-21 20:55:30 +0300

    Логгер

--- src/BvLogger.js
+++ src/BvLogger.js
@@ -19,31 +19,21 @@ class BvLogger {
      * @param {boolean=} enabled Выводит сообщения в консоль. По-умолчанию: TRUE.
      */
     constructor(name, enabled = true) {
-       this.name = name;
-       this.enabled = enabled;
+        this.name = name;
+        this.enabled = enabled;
+
+        const fmtTimestamp = {
+            toString: () => new Date().toUTCString(),
+        }
+
+        const format = '%s :: [%s] %s ::';
+
+        this.log = console.log.bind(window.console, format, ...[fmtTimestamp, 'LOG', this.name]);
+        this.error = console.error.bind(window.console, format, ...[fmtTimestamp, 'ERR', this.name]);
+        this.debug = console.debug.bind(window.console, format, ...[fmtTimestamp, 'DBG', this.name]);
+        this.trace = console.trace.bind(window.console, format, ...[fmtTimestamp, 'TRC', this.name]);
+        this.warn = console.warn.bind(window.console, format, ...[fmtTimestamp, 'WRN', this.name]);
+        this.info = console.info.bind(window.console, format, ...[fmtTimestamp, 'INF', this.name]);
     }
 
-    /**
-     * @param {string} message
-     * @returns {void}
-     */
-    log(message) {
-        if (this.enabled) {
-            /** @type {string} */
-            const msg = `${new Date().toUTCString()} :: ${this.name} :: ${message}`;
-            console.log(msg);
-        }
-    }
-
-    /**
-     * @param {string} message
-     * @returns {void}
-     */
-    error(message) {
-        if (this.enabled) {
-            /** @type {string} */
-            const msg = `${new Date().toUTCString()} :: ${this.name} :: ${message}`;
-            console.error(msg);
-        }
-    }
 }
