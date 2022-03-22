503f43d009e8e87b398da10a41f1f9251dcd2a40
Banko Viktor <bankviktor14@gmail.com>
2022-03-22 21:13:01 +0300

    FIX: отключение логгера

--- src/BvLogger.js
+++ src/BvLogger.js
@@ -9,31 +9,43 @@ class BvLogger {
     name;
 
     /**
-     * Состояние логгера.
-     * @type {boolean}
-     */
-    enabled;
-
-    /**
      * @param {string} name Название логгера.
      * @param {boolean=} enabled Выводит сообщения в консоль. По-умолчанию: TRUE.
      */
     constructor(name, enabled = true) {
         this.name = name;
-        this.enabled = enabled;
 
         const fmtTimestamp = {
             toString: () => new Date().toUTCString(),
         }
 
+        /** @type {string} */
         const format = '%s :: [%s] %s ::';
+        const dummy = function(){}
 
-        this.log = console.log.bind(window.console, format, ...[fmtTimestamp, 'LOG', this.name]);
-        this.error = console.error.bind(window.console, format, ...[fmtTimestamp, 'ERR', this.name]);
-        this.debug = console.debug.bind(window.console, format, ...[fmtTimestamp, 'DBG', this.name]);
-        this.trace = console.trace.bind(window.console, format, ...[fmtTimestamp, 'TRC', this.name]);
-        this.warn = console.warn.bind(window.console, format, ...[fmtTimestamp, 'WRN', this.name]);
-        this.info = console.info.bind(window.console, format, ...[fmtTimestamp, 'INF', this.name]);
+        this.log = enabled
+            ? console.log.bind(window.console, format, ...[fmtTimestamp, 'LOG', this.name])
+            : dummy;
+
+        this.error = enabled
+            ? console.error.bind(window.console, format, ...[fmtTimestamp, 'ERR', this.name])
+            : dummy;
+
+        this.debug = enabled
+            ? console.debug.bind(window.console, format, ...[fmtTimestamp, 'DBG', this.name])
+            : dummy;
+
+        this.trace = enabled
+            ? console.trace.bind(window.console, format, ...[fmtTimestamp, 'TRC', this.name])
+            : dummy;
+
+        this.warn = enabled
+            ? console.warn.bind(window.console, format, ...[fmtTimestamp, 'WRN', this.name])
+            : dummy;
+
+        this.info = enabled
+            ? console.info.bind(window.console, format, ...[fmtTimestamp, 'INF', this.name])
+            : dummy;
     }
 
 }
