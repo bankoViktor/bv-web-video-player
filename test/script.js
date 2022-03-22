e925b09f67fad1eb11e087122a5e76f564113dcc
Banko Viktor <bankviktor14@gmail.com>
2022-03-22 21:13:46 +0300

    Имитация загрузки эпизодов

--- test/script.js
+++ test/script.js
@@ -6,7 +6,7 @@ const player = document.querySelector(BV_VIDEO_PLAYER_TAG_NAME);
 const episodes = [];
 
 document.querySelector('#episodeClear').addEventListener('click', e => {
-    //player.appendEpisodes();
+    player.removeEpisodes();
     episodes.length = 0;
 });
 
@@ -21,5 +21,77 @@ document.querySelector('#episodeAdd').addEventListener('click', () => {
 
     episodes.push(episode);
 
-    player.appendEpisodes(episodes);
+    player.setEpisodes(episodes);
+});
+
+/**
+ * Имитация запроса списка эпизодов у сервера.
+ * @async
+ * @returns {Promise<EpisodeInfo[]>}
+ */
+const fetchEpisodesAsync = function() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            /** @type {number} */
+            const duration = 596.474195;
+            /** @type {EpisodeInfo[]} */
+            const episodes = [
+                {
+                    title: 'Вступление',
+                    duration: duration * 0.01,
+                },
+                {
+                    title: 'Приветствие',
+                    duration: duration * 0.04,
+                },
+                {
+                    title: 'Часть #1',
+                    duration: duration * 0.12,
+                },
+                {
+                    title: 'Часть #2',
+                    duration: duration * 0.13,
+                },
+                {
+                    title: 'Реклама',
+                    duration: duration * 0.02,
+                },
+                {
+                    title: 'Часть #3',
+                    duration: duration * 0.14,
+                },
+                {
+                    title: 'Часть #4',
+                    duration: duration * 0.12,
+                },
+                {
+                    title: 'Реклама',
+                    duration: duration * 0.018,
+                },
+                {
+                    title: 'Часть #5',
+                    duration: duration * 0.14,
+                },
+                {
+                    title: 'Часть #6',
+                    duration: duration * 0.16,
+                },
+                {
+                    title: 'Заключение',
+                    duration: duration * 0.07,
+                },
+                {
+                    title: 'Рекомендации',
+                    duration: duration * 0.032,
+                },
+            ];
+            resolve(episodes);
+        }, 2000);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    /** @type {EpisodeInfo[]} */
+    const episodeInfos = await fetchEpisodesAsync();
+    player.setEpisodes(episodeInfos);
 });
